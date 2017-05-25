5a99b0c3e26b0a8255076574b1e5dce49945997d
gk0909c <gk0909c@gmail.com>
2017-05-25 15:29:46 +0900

    add plugin setting

--- init.rb
+++ init.rb
@@ -4,4 +4,20 @@ Redmine::Plugin.register :redmine_gitlab_omniauth do
   description 'Omniauth to gitlab from redmine'
   version '0.0.1'
   url 'https://github.com/gk0909c/redmine_gitlab_omniauth.git'
+
+  settings default: { empty: false },
+           partial: 'settings/gitlab_setting'
+end
+
+setting = Setting['plugin_redmine_gitlab_omniauth']
+
+unless setting['app_id']
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :gitlab, setting['app_id'], setting['secret'],
+             client_options: {
+               site: setting['url'],
+               authorize_url: '/oauth/authorize',
+               token_url: '/oauth/token'
+             }
+  end
 end
